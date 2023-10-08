29e86910bf5272db69af1c6a4c875695e406b74c
Niftaliev Rustam <rustam.niftaliev97@gmail.com>
2023-10-08 08:42:38 +0400

    Project work C#

--- ProjectWorkOOP/ProjectWorkOOP/FileUsing.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include "wallet.h"
-#include <json.hpp>
-#include <fstream>
-
-int loadetLen(int& wallet_count);
-int loadWalletData(Wallet*& wallet, int& wallet_count);
--- ProjectWorkOOP/ProjectWorkOOP/wallet_space.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "myFunctions.h";
-#include "wallet.h"
-void wallet_space(Wallet* wallet, int wallet_count);
